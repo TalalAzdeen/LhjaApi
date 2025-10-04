0e8158784f865d2b548892562032afd8e7b5e43f
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-10-05 00:30:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from fastapi.responses import RedirectResponse
 from  encrypt  import * 
  
 
- 
 app = FastAPI()
 company_handler = UserHandler() 
 app.include_router(company_handler.get_router(), prefix="/company", tags=["Company"])
