4f04367813c6cd07e58a29e0c9af89568aada6f8
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-10-05 00:22:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from  encrypt  import *
 
  
 app = FastAPI()
-company_handler = CompanyHandler() 
+company_handler = UserHandler() 
 app.include_router(company_handler.get_router(), prefix="/company", tags=["Company"])
  
