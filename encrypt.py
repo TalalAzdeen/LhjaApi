745a8245245a3ec10bd0a42d28d7974482207397
TalalAzdeen <azadeentalal774355690@gmail.com>
2025-10-09 22:56:47 +0300

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -51,6 +51,7 @@ class UserHandler:
         self.router = APIRouter()
         self.db_json = self.load_db()
         self.db = CompanyDB("LhjaAPIDb.db")
+        self.db1 = SessionDB("LhjaAPIDb.db")
         
         @self.router.post("/sessions/")
         def create_session(session: SessionCreate):
